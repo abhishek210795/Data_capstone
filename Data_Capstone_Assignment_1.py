1db42b825b24f4b1396a82a72c7fa45d32a037f8
abhishek210795 <53833876+abhishek210795@users.noreply.github.com>
2019-08-07 20:21:09 +0530

    Add files via upload

---
+++ Data_Capstone_Assignment_1.py
@@ -0,0 +1,22 @@
+
+# coding: utf-8
+
+# # Data Capstone Assignment
+
+# In[1]:
+
+
+import pandas as pd
+
+
+# In[2]:
+
+
+import numpy as np
+
+
+# In[3]:
+
+
+print('Hello Capstone Project Course')
+
